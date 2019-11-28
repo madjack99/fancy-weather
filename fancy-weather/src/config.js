1b3afe7735e3fd281f385f7baac58d8b6064b281
madjack <kavanokami@gmail.com>
2019-11-28 14:58:34 +0400

    feat: connect to open cage location api

--- fancy-weather/src/config.js
+++ fancy-weather/src/config.js
@@ -1,5 +1,6 @@
 const config = {
-  darkSkyKey: '81b2d0fb1827312823a39bb72eeb1168'
+  darkSkyKey: '81b2d0fb1827312823a39bb72eeb1168',
+  openCageKey: '8a7de5baa1f64b94bd18e0baac2bb19e'
 };
 
 export default config;
--- fancy-weather/src/eventListeners.js
+++ fancy-weather/src/eventListeners.js
@@ -1,5 +1,18 @@
+import config from './config';
+
+const { openCageKey } = config;
+
 export const handleCitySubmit = e => {
-  const input = document.querySelector('.input');
   e.preventDefault();
-  console.log(input.value);
+  const input = document.querySelector('.input');
+  const city = input.value;
+  fetch(
+    `https://api.opencagedata.com/geocode/v1/json?q=${city}&key=${openCageKey}&no_annotations=1&limit=1`
+  )
+    .then(res => res.json())
+    .then(data => {
+      const { geometry } = data.results[0];
+      const { lat, lng } = geometry;
+      console.log(lat, lng);
+    });
 };
