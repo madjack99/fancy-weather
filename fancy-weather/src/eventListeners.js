409ac74a1908c03ff5ef8fdeaba01b4c85ef084d
madjack <kavanokami@gmail.com>
2019-11-29 15:23:24 +0400

    feat: show city name for selected cities

--- fancy-weather/src/eventListeners.js
+++ fancy-weather/src/eventListeners.js
@@ -2,6 +2,7 @@ import config from './config';
 import getWeather from './weather';
 import showWeather from './showWeather';
 import showTime from './showTime';
+import { insertDataIntoNode } from './helper';
 
 const { openCageKey } = config;
 
@@ -11,11 +12,14 @@ function getCoordsFromInput(input) {
   )
     .then(res => res.json())
     .then(data => {
-      const { geometry, annotations } = data.results[0];
+      const { geometry, annotations, formatted } = data.results[0];
       const { lat, lng } = geometry;
       const timeZone = annotations.timezone.name;
+      const cityName = formatted;
       console.log(data);
+      console.log(cityName);
       return {
+        cityName,
         timeZone,
         coordinates: `${lat},${lng}`
       };
@@ -27,13 +31,14 @@ export const handleCitySubmit = async e => {
 
   const input = document.querySelector('.input');
   const city = input.value;
-  const { coordinates, timeZone } = await getCoordsFromInput(city);
+  const { coordinates, timeZone, cityName } = await getCoordsFromInput(city);
   const weatherData = await getWeather(coordinates);
   const selectedLocationDateString = new Date().toLocaleString('en-US', {
     timeZone
   });
   const dateObj = new Date(selectedLocationDateString);
 
+  insertDataIntoNode(cityName, '.location-name');
   showTime(dateObj);
   showWeather(weatherData);
 };
